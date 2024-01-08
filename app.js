ab74ddad7278a51c9fd9c933d346fc2b4af2e879
Jasoonii <jason.ddang1@gmail.com>
2024-01-08 17:02:51 -0600

    Pushing project to main

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var redirect_uri = "http://127.0.0.1:5500/home.html";
+var redirect_uri = "/home.html";
 
 var clientID = "7c93cb9ea0c8476cbae408755dfe92fa";
 var clientSecret = "8e14ff226cc943989ae9de4846a1fb70";
